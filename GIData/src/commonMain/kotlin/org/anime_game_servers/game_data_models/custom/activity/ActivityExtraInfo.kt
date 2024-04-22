5fc6ca605497e3043eed3b5c1c5b04e2adaaa50e
hartie95 <mail@hartie95.de>
2024-04-22 18:24:50 +0200

    [GI] Addeded some more ActivityConditions

--- GIData/src/commonMain/kotlin/org/anime_game_servers/game_data_models/custom/activity/ActivityExtraInfo.kt
+++ GIData/src/commonMain/kotlin/org/anime_game_servers/game_data_models/custom/activity/ActivityExtraInfo.kt
@@ -24,9 +24,9 @@ import org.anime_game_servers.game_data_models.loader.FolderType
  * @property activeConditions list of conditions that must be met for the activity to be active and playable
  * @property comment additional comment about either the activity or the data in it, only relevant for the humans interacting with the data
  */
-@DataFile("Server/Activity/ActivityInfo.json", FileType.JSON, FolderType.EXCEL)
+@DataFile("Server/Activity/ActivityExtraInfo.json", FileType.JSON, FolderType.EXCEL)
 @Serializable
-data class ActivityInfo(
+data class ActivityExtraInfo(
     val activityId: Int,
     val duration: Int = UnsetInt,
     val rewardPreview: Int = UnsetInt,
